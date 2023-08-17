26a64325fac0bcf2073e59f7b728d66039b8d385
KnotSarvajit <sarvajitvisagan@gmail.com>
2023-08-18 01:09:20 +0530

    new api endpoint tested and discover works progressed

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl:'https://shazam-core7.p.rapidapi.com',
+        baseUrl:'https://shazam-core.p.rapidapi.com/v1',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', 'c8d4239becmsh894462841aeb492p147263jsn36dd92cb7d18');
 
@@ -12,7 +12,7 @@ export const shazamCoreApi = createApi({
     }),
 
     endpoints: (builder) =>({
-        getTopCharts: builder.query({query: () => '/charts/get-top-songs-in-world'}),
+        getTopCharts: builder.query({query: () => '/charts/world'}),
     })
 });
 
