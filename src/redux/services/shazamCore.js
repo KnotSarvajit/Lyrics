264307aac8a8325eef0e53d57d19327190608051
KnotSarvajit <sarvajitvisagan@gmail.com>
2023-09-13 21:16:04 +0530

    search implementation, other pages

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl:'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl:'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', 'c8d4239becmsh894462841aeb492p147263jsn36dd92cb7d18');
 
@@ -12,14 +12,22 @@ export const shazamCoreApi = createApi({
     }),
 
     endpoints: (builder) =>({
-        getTopCharts: builder.query({query: () => '/charts/world'}),
-        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
-        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+        getTopCharts: builder.query({query: () => 'v1/charts/world'}),
+        getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+        getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
     }),
 });
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsBySearchQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
+    useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery,
 } = shazamCoreApi;
